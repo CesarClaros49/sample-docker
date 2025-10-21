de62b2b65d92b71ad3bf2e0a1beb095dbb71bda4
Clarozzz <cesar.claros@unah.hn>
2025-10-20 18:31:56 -0600

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     port: 3000,
-    host: true,
+    allowedHosts: [".sslip.io"],
     proxy:  {
       '/api/todos': {
         target: backendUrl,
