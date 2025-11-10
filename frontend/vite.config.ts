a47cdc9ba0dae8593cf89bd9259c6158acb27358
CesarClaros49 <cesar.claros@unah.hn>
2025-11-10 11:26:31 -0600

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     port: 3000,
-    allowedHosts: [".sslip.io", ".amazonaws.com"],
+    allowedHosts: [".sslip.io", "k8s-test-frontend-a2de45414a-3ff9da152f3938cc.elb.us-east-1.amazonaws.com"],
     proxy:  {
       '/api/todos': {
         target: backendUrl,
